70500c9170fbbaa4194440174b08836044a2c086
carolina <carolina@sindominio.net>
2014-12-15 12:00:30 +0100

    Add routing: routeProvider and httpProvider

--- files/org/digitalmemex/webclient/scripts/app.js
+++ files/org/digitalmemex/webclient/scripts/app.js
@@ -1,18 +1,43 @@
-/*
-ViewModel
-    state - The state the application is in, there are 4 states: "initial", "category list", "geo object list", "geo object details"
-    categoryLayers - An object that contains the category topic, the geo object markers layer, the visibility of the layer
-    criteria - the criteria shown upper/right (array of topics)
-    currentCriteria - selected criteria (topic)
-    criteriaCategories - the categories shown in the lower/right (array of topics)
-    geoObjects - the geoObjects of a specific category shown in the lower/right
-    detailGeoObject - the details of a geoObject
-    currentCategory - the selected category (topic)
-    details - the details values of a geoObject (object)
-    geoObjCategories - the categories of a geoObject shown in geo object details (array)
-    markersLayer - object with the markers of a specific category
-*/
+
+var app = angular.module('tornarFrontend', ['ngSanitize','ngRoute']);
+
+app.config(function ($routeProvider) {
+      console.log("Configuring routes")
+      $routeProvider
+            .when('/', {
+                templateUrl: '/filerepo/org/digitalmemex/webclient/views/home.html'
+            })
+            .when('/map', {
+                templateUrl: '/filerepo/org/digitalmemex/webclient/views/map.html',
+                controller: 'sidebarController'
+            })
+            .otherwise({redirectTo: "/"})
+    });
 
 
-var app = angular.module('kiezatlasFrontend', ['ngSanitize']);
+app.config(function($httpProvider) {
+    $httpProvider.interceptors.push(function ($q){
+        console.log("httpProvider");
+        return {
+            request: function (config) {
+                console.log(config);
+                return config;
+            },
+            response: function (result) {
+                console.log("This is the response");
+                return result;
+            },
+            responseError: function (error) {
+                console.log("Failed with", error.status, "status");
+                return $q.reject(error);
+            }
+        }
+        
+    });
+});
+
+        
+    
+                                   
+     
 
